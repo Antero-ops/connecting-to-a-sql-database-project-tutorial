8715ad306c899dc61e21192c430b385dace69518
Antero-ops <anterovasquez24@gmail.com>
2024-10-30 15:24:00 +0000

    casi acabado

--- src/app.py
+++ src/app.py
@@ -10,8 +10,10 @@ load_dotenv()
 connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 engine = create_engine(connection_string).execution_options(autocommit=True)
 engine.connect()
+
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
-create_engine = {"""CREATE TABLE publishers(
+engine.execute = ("""
+CREATE TABLE publishers(
     publisher_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY(publisher_id)
@@ -43,10 +45,10 @@ CREATE TABLE book_authors (
     PRIMARY KEY(book_id, author_id),
     CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
     CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
-);"""
-}
+);""")
+
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
-engine.execute = {"""-- publishers
+engine.execute = ("""-- publishers
 INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
 INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS');
@@ -87,10 +89,12 @@ INSERT INTO book_authors (book_id, author_id) VALUES (6, 4);
 INSERT INTO book_authors (book_id, author_id) VALUES (7, 3);
 INSERT INTO book_authors (book_id, author_id) VALUES (8, 2);
 INSERT INTO book_authors (book_id, author_id) VALUES (9, 4);
-INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);"""
-}
+INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);
+""")
+
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
 
-dataframe = pd.read_sql("SELECT publisher_id from authors", engine.connect)
+
+dataframe = pd.read_sql("Select * from publishers;", engine)
 
 print(dataframe)
